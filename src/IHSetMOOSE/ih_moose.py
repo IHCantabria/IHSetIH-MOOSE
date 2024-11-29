62926dafa0e924dc4973ee8d6114c166f9f0901b
CB-Lim <129813815+CB-Lim@users.noreply.github.com>
2024-11-29 10:43:31 +0100

    Update ih_moose.py

--- src/IHSetMOOSE/ih_moose.py
+++ src/IHSetMOOSE/ih_moose.py
@@ -1,85 +1,68 @@
-import os
 import xarray as xr
 import numpy as np
+import json
+
+from IHSetMillerDean import calibration_2 as cal_MD
+from IHSetYates09 import calibration_2 as cal_YA
+from IHSetShoreFor import calibration_2 as cal_SF
+from IHSetJara import calibration_2 as cal_JR
+from IHSetJaramillo20 import calibration_2 as cal_JA20
+from IHSetLim import calibration_2 as cal_LIM
+
+from IHSetTurki import calibration_2 as cal_TU
+from IHSetJaramillo21a import calibration_2 as cal_JA21a
+
 from .ih_moose_jit import ih_moose_jit_par1, ih_moose_jit_par2, gonzalez_ih_moose, initialize_array, intersect_with_min_distance
 
 class ih_moose(object):
     
-    def __init__(self, path_prof, model, **kwargs):
+    def __init__(self, path, planform, model):
         """
         Jaramillo et al. 2021 model
         """
-        S = model.cross.full_run
-        alp = model.long.full_run
-
-        lim = kwargs['lim']
-        Fmean = kwargs['Fmean']
-        T = kwargs['T']
-        depth = kwargs['depth']
-        parabola_num = kwargs['parabola_num']
-        trs_S = kwargs['trs_S']
-        self.parabola_num = parabola_num
+        self.path = path
+        data = xr.open_dataset(path)
+        self.trs = model.trs
+                
+        S = model.cross_run
+        alp = model.long_run
         
-        ens = xr.open_dataset(os.path.join(path_prof))
-        self.Obs = ens['obs'].values
-        self.time_obs = ens['time_obs'].values
-        self.ntrs_xi = ens['xi'].values
-        self.ntrs_yi = ens['yi'].values
-        self.ntrs_xf = ens['xf'].values
-        self.ntrs_yf = ens['yf'].values
-        ntrs = ens['ntrs'].values
-        self.ntrs = ntrs[-1] + 1
+        if self.trs == 'Average':
+            print('Please select the specific transect')
+        else:
+            self.Obs = data.obs.values
+            self.ntrs_xi = data.xi.values
+            self.ntrs_yi =  data.yi.values
+            self.ntrs_xf =  data.xf.values
+            self.ntrs_yf =  data.yf.values
+            ntrs =  data.ntrs.values
+            self.ntrs = ntrs[-1] + 1
         
-        DirN = 90 - np.arctan((self.ntrs_yi[trs_S-1] - self.ntrs_yf[trs_S-1]) / (self.ntrs_xi[trs_S-1] - self.ntrs_xf[trs_S-1])) * 180 / np.pi
-        self.Cl = [self.ntrs_xi[trs_S-1], self.ntrs_yi[trs_S-1]]
+        mkIdx = np.vectorize(lambda t: np.argmin(np.abs(model.cross.time - t)))
+        self.idx_obs = mkIdx(model.cross.time_obs)
+                
+        DirN = 90 - np.arctan((self.ntrs_yi[self.trs] - self.ntrs_yf[self.trs]) / (self.ntrs_xi[self.trs] - self.ntrs_xf[self.trs])) * 180 / np.pi
+        self.Cl = [self.ntrs_xi[self.trs], self.ntrs_yi[self.trs]]
         
-        Dif = np.abs(DirN - Fmean)
+        Dif = np.abs(DirN - planform.Fmean)
         dX = - (S * np.cos(np.radians(Dif)))
         
         delta_alpha = alp - np.mean(alp)
         delta_alpha = delta_alpha * np.pi / 180
     
-        if parabola_num == 1:
-            self.Cp = kwargs['Cp']
-            self.Lr = kwargs['Lr']
-            self.npro = kwargs['npro']
-            
-            self.xi, self.yi, self.xf, self.yf = ih_moose.transects(self.npro, lim[0], lim[1], self.Cl, Fmean, self.Cp, 0, 0, self.Lr, T, depth, parabola_num)
-            prof = np.zeros((self.npro, 5))
-            
-            prof[:, 0] = 90 - np.arctan((self.yi - self.yf) / (self.xi - self.xf)) * 180 / np.pi
-            prof[:, 1] = self.xi
-            prof[:, 2] = self.yi
-            prof[:, 3] = self.xf
-            prof[:, 4] = self.yf
-            
-            xyi = np.vstack((self.xi, self.yi))
-            pivot_point = np.array([ens['x_pivotal'], ens['y_pivotal']]).reshape(2, 1)
+        if planform.parabola_num == 1:                        
+            xyi = np.vstack((planform.prof[:, 1], planform.prof[:, 2]))
+            pivot_point = np.array([data.x_pivotal.values, data.x_pivotal.values]).reshape(2, 1)
             pivotN = np.argmin(np.linalg.norm(xyi.T - pivot_point.T, axis=1))
             
-            self.prof = prof
-            self.S_PF, self.costas_x, self.costas_y = ih_moose_jit_par1(prof, pivotN, Fmean, self.Cp, self.Cl, T, depth, self.Lr, dX, delta_alpha, 0)
+            self.S_PF, self.costas_x, self.costas_y = ih_moose_jit_par1(planform.prof, pivotN, planform.Fmean, planform.Cp, self.Cl, planform.T, planform.depth, planform.Lr, dX, delta_alpha, 0)
         
-        if parabola_num == 2:
-            self.Cp1 = kwargs['Cp1']
-            self.Cp2 = kwargs['Cp2']
-            self.npro = kwargs['npro']
-            
-            self.xi, self.yi, self.xf, self.yf = ih_moose.transects(self.npro, lim[0], lim[1], self.Cl, Fmean, 0, self.Cp1, self.Cp2, 0, T, depth, parabola_num)
-            prof = np.zeros((self.npro, 5))
-            
-            prof[:, 0] = 90 - np.arctan((self.yi - self.yf) / (self.xi - self.xf)) * 180 / np.pi
-            prof[:, 1] = self.xi
-            prof[:, 2] = self.yi
-            prof[:, 3] = self.xf
-            prof[:, 4] = self.yf
-            
-            xyi = np.vstack((self.xi, self.yi))
-            pivot_point = np.array([ens['x_pivotal'], ens['y_pivotal']]).reshape(2, 1)
+        if planform.parabola_num == 2:
+            xyi = np.vstack((planform.prof[:, 1], planform.prof[:, 2]))
+            pivot_point = np.array([data.x_pivotal.values, data.x_pivotal.values]).reshape(2, 1)
             pivotN = np.argmin(np.linalg.norm(xyi.T - pivot_point.T, axis=1))
             
-            self.prof = prof
-            self.S_PF, self.costas_x, self.costas_y = ih_moose_jit_par2(prof, pivotN, Fmean, self.Cp1, self.Cp2, self.Cl, T, depth, 0, dX, delta_alpha, 0)
+            self.S_PF, self.costas_x, self.costas_y = ih_moose_jit_par2(planform.prof, pivotN, planform.Fmean, planform.Cp1, planform.Cp2, self.Cl, planform.T, planform.depth, 0, dX, delta_alpha, 0)
                 
         self.SS = initialize_array((len(dX), self.ntrs), np.float64)
         
@@ -90,62 +73,148 @@ class ih_moose(object):
                 xf, yf = intersect_with_min_distance(m, b, self.costas_x[i,:], self.costas_y[i,:])
                 squared_distance = ((xf - self.ntrs_xi[j])**2 + (yf - self.ntrs_yi[j])**2)
                 self.SS[i, j] = np.sqrt(squared_distance)
-        
-    def transects(nTrs, pt1, pt2, xyi, Fmean, Cp, Cp1, Cp2, Lr, T, depth, parabola_num):
-        
-        if parabola_num == 1:
-            Cl = xyi
-            costa_xi, costa_yi = gonzalez_ih_moose(Fmean, Cp, Cl, T, depth, Lr, 0, 300)
-            costa_xf, costa_yf = gonzalez_ih_moose(Fmean, Cp, Cl, T, depth, Lr, 0, -300)
             
-        if parabola_num == 2:
-            Cl = xyi
-            Lr = 0
-            costa_xi1, costa_yi1 = gonzalez_ih_moose(Fmean, Cp1, Cl, T, depth, Lr, 0, 300)
-            costa_xi2, costa_yi2 = gonzalez_ih_moose(Fmean, Cp2, Cl, T, depth, Lr, 0, 300)
-            costa_xi = combine_arrays(costa_xi1, costa_xi2)
-            costa_yi = combine_arrays(costa_yi1, costa_yi2)
-        
-            costa_xf1, costa_yf1 = gonzalez_ih_moose(Fmean, Cp1, Cl, T, depth, Lr, 0, -300)
-            costa_xf2, costa_yf2 = gonzalez_ih_moose(Fmean, Cp2, Cl, T, depth, Lr, 0, -300)
-            costa_xf = combine_arrays(costa_xf1, costa_xf2)
-            costa_yf = combine_arrays(costa_yf1, costa_yf2)
+class equilibrium_planform(object):
     
-        Fmeand = Fmean * np.pi/180
+    def __init__(self, path):
+        """
+        Jaramillo et al. 2021 model
+        """
+        self.path = path
+        data = xr.open_dataset(path)
+        cfg = json.loads(data.attrs['IH_MOOSE'])
+        
+        self.lim = cfg['lim']
+        self.Fmean = cfg['Fmean']
+        self.T = cfg['T']
+        self.depth = cfg['hd']
+        self.parabola_num = cfg['parabola_num']
+        
+        self.npro = cfg['npro']
+        self.lpro = cfg['lpro']
+        self.prof = np.zeros((self.npro, 5))
+        
+        self.Cl = cfg['Cl']
+        
+        if self.parabola_num == 1:
+            self.Cp = cfg['Cp']
+            self.Lr = cfg['Lr']
+            self.costa_xe, self.costa_ye, self.alpha_curve = gonzalez_ih_moose(self.Fmean, self.Cp, self.Cl, self.T, self.depth, self.Lr, 0, 0)
+            self.costa_xi, self.costa_yi, _ = gonzalez_ih_moose(self.Fmean, self.Cp, self.Cl, self.T, self.depth, self.Lr, 0, self.lpro/2)
+            self.costa_xf, self.costa_yf, _ = gonzalez_ih_moose(self.Fmean, self.Cp, self.Cl, self.T, self.depth, self.Lr, 0, -self.lpro/2)
+            
+        if self.parabola_num == 2:
+            self.Cp1 = cfg['Cp1']
+            self.Cp2 = cfg['Cp2']
+            costa_xe1, costa_ye1, self.alpha_curve1 = gonzalez_ih_moose(self.Fmean, self.Cp1, self.Cl, self.T, self.depth, 0, 0, 0)
+            costa_xe2, costa_ye2, self.alpha_curve2 = gonzalez_ih_moose(self.Fmean, self.Cp2, self.Cl, self.T, self.depth, 0, 0, 0)
+            self.costa_xe = combine_arrays(costa_xe1, costa_xe2)
+            self.costa_ye = combine_arrays(costa_ye1, costa_ye2)
+            
+            costa_xi1, costa_yi1, _ = gonzalez_ih_moose(self.Fmean, self.Cp1, self.Cl, self.T, self.depth, 0, 0, self.lpro/2)
+            costa_xi2, costa_yi2, _ = gonzalez_ih_moose(self.Fmean, self.Cp2, self.Cl, self.T, self.depth, 0, 0, self.lpro/2)
+            self.costa_xi = combine_arrays(costa_xi1, costa_xi2)
+            self.costa_yi = combine_arrays(costa_yi1, costa_yi2)
+        
+            costa_xf1, costa_yf1, _ = gonzalez_ih_moose(self.Fmean, self.Cp1, self.Cl, self.T, self.depth, 0, 0, -self.lpro/2)
+            costa_xf2, costa_yf2, _ = gonzalez_ih_moose(self.Fmean, self.Cp2, self.Cl, self.T, self.depth, 0, 0, -self.lpro/2)
+            self.costa_xf = combine_arrays(costa_xf1, costa_xf2)
+            self.costa_yf = combine_arrays(costa_yf1, costa_yf2)
+    
+        Fmeand = self.Fmean * np.pi/180
         rotation_matrix = np.array([[np.cos(Fmeand), -np.sin(Fmeand)],
                                 [np.sin(Fmeand), np.cos(Fmeand)]])
         reverse_rotation_matrix = np.array([[np.cos(-Fmeand), -np.sin(-Fmeand)],
                                         [np.sin(-Fmeand), np.cos(-Fmeand)]])
     
         # Process initial points (i)
-        original_points_i = np.vstack((costa_xi, costa_yi))
-        closestPt1i = np.argmin(np.linalg.norm(original_points_i.T - pt1, axis=1))
-        closestPt2i = np.argmin(np.linalg.norm(original_points_i.T - pt2, axis=1))
+        original_points_i = np.vstack((self.costa_xi, self.costa_yi))
+        closestPt1i = np.argmin(np.linalg.norm(original_points_i.T - self.lim[0], axis=1))
+        closestPt2i = np.argmin(np.linalg.norm(original_points_i.T - self.lim[1], axis=1))
     
         rotated_points_i = rotation_matrix.dot(original_points_i[:,min(closestPt1i,closestPt2i):max(closestPt1i,closestPt2i)])
         rot_xi, rot_yi = rotated_points_i
         sorted_indices_i = np.argsort(rot_xi)
-        xi = np.linspace(np.min(rot_xi), np.max(rot_xi), nTrs)
+        xi = np.linspace(np.min(rot_xi), np.max(rot_xi), self.npro)
         yi = np.interp(xi, rot_xi[sorted_indices_i], rot_yi[sorted_indices_i])
         rotated_points_array_i = np.vstack((xi, yi))
         reverted_points_i = reverse_rotation_matrix.dot(rotated_points_array_i)
-        xi, yi = reverted_points_i
+        self.xi, self.yi = reverted_points_i
 
         # Process final points (f)
-        original_points_f = np.vstack((costa_xf, costa_yf))    
-        closestPt1f = np.argmin(np.linalg.norm(original_points_f.T - pt1, axis=1))
-        closestPt2f = np.argmin(np.linalg.norm(original_points_f.T - pt2, axis=1))
+        original_points_f = np.vstack((self.costa_xf, self.costa_yf))    
+        closestPt1f = np.argmin(np.linalg.norm(original_points_f.T - self.lim[0], axis=1))
+        closestPt2f = np.argmin(np.linalg.norm(original_points_f.T - self.lim[1], axis=1))
     
         rotated_points_f = rotation_matrix.dot(original_points_f[:,min(closestPt1f,closestPt2f):max(closestPt1f,closestPt2f)])
         rot_xf, rot_yf = rotated_points_f
         sorted_indices_f = np.argsort(rot_xf)
-        xf = np.linspace(np.min(rot_xf), np.max(rot_xf), nTrs)
+        xf = np.linspace(np.min(rot_xf), np.max(rot_xf), self.npro)
         yf = np.interp(xf, rot_xf[sorted_indices_f], rot_yf[sorted_indices_f])
         rotated_points_array_f = np.vstack((xf, yf))
         reverted_points_f = reverse_rotation_matrix.dot(rotated_points_array_f)
-        xf, yf = reverted_points_f
+        self.xf, self.yf = reverted_points_f
+       
+        self.prof[:, 0] = 90 - np.arctan((self.yi - self.yf) / (self.xi - self.xf)) * 180 / np.pi
+        self.prof[:, 1] = self.xi
+        self.prof[:, 2] = self.yi
+        self.prof[:, 3] = self.xf
+        self.prof[:, 4] = self.yf
+
+
+class cal_IH_MOOSE(object):
+    """
+    cal_IH_MOOSE
     
-        return xi, yi, xf, yf
+    Configuration to calibfalse,and run the Jaramillo et al. (2020) Hybrid Model.
+    
+    This class reads input datasets, performs its calibration.
+    """
+
+    def __init__(self, path):
+        
+        self.path = path
+        data = xr.open_dataset(path)
+        cfg = json.loads(data.attrs['IH_MOOSE'])
+        
+        self.crossshore = cfg['crossshore']
+        self.longshore = cfg['longshore']
+        
+        if self.crossshore == 'MD':
+            cfg_c = json.loads(data.attrs['MillerDean'])
+            self.trs = cfg_c['trs']
+            self.cross = cal_MD.cal_MillerDean_2(self.path)
+        if self.crossshore == 'YA':
+            cfg_c = json.loads(data.attrs['Yates09'])
+            self.trs = cfg_c['trs']
+            self.cross = cal_YA.cal_Yates09_2(self.path)
+        if self.crossshore == 'SF':
+            cfg_c = json.loads(data.attrs['ShoreFor'])
+            self.trs = cfg_c['trs']
+            self.cross = cal_SF.cal_ShoreFor_2(self.path)
+        if self.crossshore == 'JR':
+            cfg_c = json.loads(data.attrs['Jara'])
+            self.trs = cfg_c['trs']
+            self.cross = cal_JR.cal_Jara_2(self.path)
+        if self.crossshore == 'JA':
+            cfg_c = json.loads(data.attrs['Jaramillo20'])
+            self.trs = cfg_c['trs']
+            self.cross = cal_JA20.cal_Jaramillo20_2(self.path)
+        if self.crossshore == 'LIM':
+            cfg_c = json.loads(data.attrs['Lim'])
+            self.trs = cfg_c['trs']
+            self.cross = cal_LIM.cal_Lim_2(self.path)
+        
+        if self.longshore == 'TU':
+            self.long = cal_TU.cal_Turki_2(self.path)
+        if self.longshore == 'JA':
+            self.long = cal_JA21a.cal_Jaramillo21a_2(self.path)
+
+        self.cross.calibrate()
+        self.cross_run = self.cross.run_model(self.cross.solution)
+        
+        self.long.calibrate()
+        self.long_run = self.long.run_model(self.long.solution)
         
 def combine_arrays(x1, x2):
     x_bt = np.linspace(x1[-1], x2[-1])
